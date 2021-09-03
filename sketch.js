a5667ca7a181a4dd401ab4669852306c3dc24684
Prankur14 <70706874+Prankur14@users.noreply.github.com>
2021-09-03 22:40:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ var iss, spacecraft;
 var bg, issimg, scimg;
 var hasDocked = false;
 var x = window.innerWidth / 2;
-var y = 500;
+var y = 800;
 var issX = window.innerWidth / 2;
 var issY = 300;
-let reloadButton;
+// let reloadButton;
 let meme;
 let memeX = -200;
 let myX;
-
+let currenturl = window.location.href;
 
 function preload() {
     bg = loadImage("space.jpg");
@@ -30,7 +30,7 @@ function setup() {
     spacecraft = createSprite(x, y);
     spacecraft.addImage(scimg);
     spacecraft.scale = 0.35;
-    // reloadButton = createButton("reload")
+    // reloadButton = createButton("This button is useless...")
     iss = createSprite(issX, issY);
     iss.addImage(issimg)
     iss.scale = 0.55;
@@ -44,6 +44,14 @@ function draw() {
     if (second() % 5 == 0) {
         fullscreen(true);
     }
+    // reloadButton.position(100, 10);
+    // reloadButton.style
+
+    // reloadButton.style('background', 'lavender');
+    // reloadButton.style('font-family', 'comfortaa');
+    // reloadButton.style('font-size', '25px');
+    // reloadButton.style('sont-style', )
+
     // fullscreen(true);
     spacecraft.addImage(scimg);
     if (!hasDocked) {
@@ -73,16 +81,23 @@ function draw() {
             spacecraft.y += 5
         }
 
-//         if (mouseIsPressed) {
-//             spacecraft.x = mouseX;
-//             spacecraft.y = mouseY;
-//         }
+        // if (mouseIsPressed) {
+        //     iss.x = mouseX;
+        //     iss.y = mouseY;
+        //     // spacecraft.x = mouseX;
+        //     // spacecraft.y = mouseY;
+        // }
+
         // document.getElementById()
 
 
     }
 
-    if (spacecraft.y <= (iss.y + 140) && spacecraft.x <= (iss.x - 15) && spacecraft.x >= iss.x - 60) {
+    // if (spacecraft.x > iss.x - 150 && spacecraft.x < iss.x + 150 && spacecraft.y > iss.y - 150 && spacecraft.y < iss.y + 250) {
+    //     reloadButton.html("But maybe.....");
+    // }
+
+    if (spacecraft.y <= (iss.y + 140) && spacecraft.y >= (iss.y + 30 - 70) && spacecraft.x <= (iss.x - 15) && spacecraft.x >= iss.x - 60) {
         hasDocked = true;
         fill("white")
         textFont("comfortaa");
@@ -90,6 +105,12 @@ function draw() {
         textSize(45);
         text("Docking Successful!", width / 2 - 200, height / 2 + 180);
         pop();
+        // reloadButton.html("I can Reload this page for you.");
+
+
+        // reloadButton.mousePressed(() => {
+        //         window.location.reload();
+        //     })
         // push();
         // textSize(15);
         // // text("This page will reload in 5 seconds.", width / 2 - 200, height / 2 + 250)
@@ -108,29 +129,27 @@ function draw() {
 
         if (meme.isPlaying() === false) {
             meme.play();
-            // meme.loop(100000);
-            // alert("hi");
-            // memeX = 10000;
-            // alert("hello");
+            meme.addCue(3, changeX);
             console.log("false");
         } else {
-            memeX += 1
-
+            memeX += 1;
             console.log("true");
             myX = memeX;
-
-
         }
 
 
-        image(memeImage, memeX, height / 2 + 100);
+        image(memeImage, memeX, height / 2);
 
     }
-    if (memeX > -95) {
-        window.location.reload();
-    }
+
 
     drawSprites();
+}
+
+function changeX() {
+    // memeX = 1000;
+    // meme.pause();
+    window.location.reload();
 }
 
 function windowResized() {
